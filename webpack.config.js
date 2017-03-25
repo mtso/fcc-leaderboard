4abfe194fc06e9d52f2ffeeeb153eb7bc235ae7e
mtso <mtso@tuta.io>
2017-03-24 23:59:02 -0700

    Enable webpack for html, javascript, and scss

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, './app/index.html'),
+  filename: 'index.html',
+  inject: 'body'
+})
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var exportDir = path.resolve(__dirname, './dist');
+
+module.exports = [
+  {
+    entry: [
+      './app/index.js'
+    ],
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'}
+      ]
+    },
+    output: {
+      filename: 'index_bundle.js',
+      path: exportDir
+    },
+    plugins: [
+      HtmlWebpackPluginConfig
+    ]
+  },
+  {
+    entry: [
+      './app/style/main.scss'
+    ],
+    module:
+    {
+      loaders: [
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader!sass-loader'}
+          )
+        }
+      ]
+    },
+    output: {
+      filename: 'style.css',
+      path: exportDir
+    },
+    plugins: [
+      new ExtractTextPlugin({
+        filename: 'style.css',
+        disable: false,
+        allChunks: true
+      })
+    ]
+  }
+];
