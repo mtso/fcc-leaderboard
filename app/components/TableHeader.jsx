2546f3a58e79b5d93737ef5e97407639a9da39a8
mtso <mtso@tuta.io>
2017-03-29 22:25:22 -0700

    Render Table

--- app/components/TableHeader.jsx
+++ app/components/TableHeader.jsx
@@ -6,8 +6,8 @@ const TableHeader = React.createClass({
       <tr>
         <th>Rank</th>
         <th>Username</th>
-        <th onclick={this.props.handleRecent}>Recent</th>
-        <th onclick={this.props.handleAlltime}>All-Time</th>
+        <th onClick={this.props.handleRecent}>Recent</th>
+        <th onClick={this.props.handleAlltime}>All-Time</th>
       </tr>
     )
   }
